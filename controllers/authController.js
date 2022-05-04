a34ca26443f8e2a08fa2557d4106a61ecc110315
Thavorac <thavorac.chun@gmail.com>
2022-05-04 11:13:17 +0700

    Structure of the project

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+exports.register = (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const gender = req.body.gender;
+  const email = req.body.email;
+
+  // Validate if password and confirm password is match
+
+  // Check if email is valid and not exist
+
+  // -- Send veriy code or link to that email
+
+  // Create user record + password hash
+
+  // response and redirect to login
+}
+
---
+++ routes/admin.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const router = express.Router();
+
+router.post("/register", authController.register)
